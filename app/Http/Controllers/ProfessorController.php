9524b4bffd5c4498403e3122eb2a9608aaaabf0e
Matheus <bonecodoido339@gmail.com>
2024-08-08 14:18:55 -0300

    Upload to git

---
+++ app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Professor;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
+
+class ProfessorController extends Controller
+{
+    protected $model; 
+
+    public function __construct(Professor $professor)
+    {
+        $this->model = $professor; 
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response($this->model->all()); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function criar(Request $request)
+    {
+        try{
+            $this->model->create($request->all());
+            return response('Criado com sucesso!'); 
+        } catch(\Throwable $th){
+            throw $th; 
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\ProfessorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/index',[ProfessorController::class, 'index'])->name('api.indexteste');
+Route::post('/criar',[ProfessorController::class, 'criar'])->name('api.criar');
